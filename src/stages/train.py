e1302dd99e00d8d44e1c0d479730e1deba64d4e5
ykasimov <yury.kasimov@gmail.com>
2022-08-13 06:26:30 +0000

    change to pytorch tripletloss

--- src/stages/train.py
+++ src/stages/train.py
@@ -11,7 +11,7 @@ import argparse
 import matplotlib
 import matplotlib.pyplot as plt
 from torch.utils.data import random_split
-
+from torch.nn import TripletMarginLoss
 from networks import EmbeddingNet, TripletNet
 from torchvision.datasets import ImageFolder
 from losses import TripletLoss
@@ -40,25 +40,25 @@ def train(teacher_area):
     triplet_dataset_train = TripletMNIST(train_dataset, train=True, transform=model.embedding_net.transforms) 
     triplet_dataset_val = TripletMNIST(val_dataset, train=False, transform=model.embedding_net.transforms)
     # triplet_test_dataset = TripletMNIST(test_dataset)
-    batch_size = 32
+    batch_size = 128
     kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
     triplet_train_loader = torch.utils.data.DataLoader(triplet_dataset_train, batch_size=batch_size, shuffle=True, **kwargs)
     triplet_test_loader = torch.utils.data.DataLoader(triplet_dataset_val, batch_size=batch_size, shuffle=False, **kwargs)
 
     # Set up the network and training parameters
 
-    margin = 1.0
+    margin = 0.1
    
 
     if cuda:
         model.cuda()
-    loss_fn = TripletLoss(margin)
-    lr = 0.001
+    loss_fn = TripletMarginLoss()
+    lr = 0.000001
     # optimizer = optim.Adam(model.parameters(), lr=lr)
     optimizer = optim.RMSprop(model.parameters(), lr=lr)
     scheduler = lr_scheduler.StepLR(optimizer, 2, gamma=0.1, last_epoch=-1)
     
-    n_epochs = 8
+    n_epochs = 20
     log_interval = 10
     fit(triplet_train_loader, triplet_test_loader,  model, loss_fn, optimizer, scheduler, n_epochs, cuda, log_interval, log_folder=f"dvclive_{teacher_area}")#, metrics=[AccumulatedAccuracyMetric()])
 
